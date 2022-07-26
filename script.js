ec902daaf25633e45aa778c7044721e67f6eb0fe
Bernardo Viero <61853561+bernardoviero@users.noreply.github.com>
2022-07-26 17:50:24 -0300

    upload js e mudança html

--- script.js
+++ script.js
@@ -1,39 +1,83 @@
 let sliderElement = document.querySelector("#slider");
-
 let sizePassword = document.querySelector("#valor");
 let password = document.querySelector("#password");
+var checkbox = document.querySelector("#checkbox")
 
-let containerPassword = document.querySelector("#container-password")
 
 
-let charsetLetra = "abcdGHIJKLMNOPQRSTUVWXYZefghGHIJKLMNOPQRSTUVWXYZijklmnopqrstuvwxyzABCDefghEFGHIJKLabcdMNOPQRSefghTUVWXYZ"
-let charsetNumero = "0123456789"
-let charsetNumeroLetra = "abcdGHIJK0123456789LMNOPQRSTUVW0123456789XYZefghGHIJ0123456789KLMNOPQRSTUVWXYZ0123456789ijklmnopqrstuvwxyzABCDefghEFGHIJK0123456789LabcdMNOPQRSefghTUVWXYZ"
-let charsetCompleted = "abcd!@#*GHIJK01234567!@#*89LMNOPQ!@#*RSTUVW0!@#*123456!@#*789XYZefghGHIJ0123456!@#*789KLMNOPQRSTUVWXYZ01234!@#*56789ijklmnopqrstuvwx!@#*yzABC!@#*DefghEFGHIJK01!@#*23456789La!@#*bcdMNOPQRSefghTUV!@#*WXYZ"
-
-let novaSenha = "";
+let letrasMaisculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let letrasMinusculas = "abcdefghijklmnopqrstuvwxyz";
+let letras = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let numeros = "0123456789";
+let caracterEspecial = "!@#&*()\/";
+let charsetCompleto = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#&*()\/"
 
 sizePassword.innerHTML = sliderElement.value;
-
 slider.oninput = function () {
     sizePassword.innerHTML = this.value;
 }
 
-function generatePassword(parametro) {
 
-    let pass = "";
-    for (let i = 0, n = parametro.length; i < (sliderElement.value); ++i) {
-        pass += parametro.charAt(Math.floor(Math.random() * n));
+//let sizeSenha = ((senha.length + 1) - senha.length);
+let Novasenha = "";
+
+
+function gerarSenhaLetras(parametro) {
+    gerarSenha(letrasMaisculas);
+    gerarSenha(letrasMinusculas);
+    gerarSenhaCompleta(letras);
+    function gerarSenha(parametro) {
+        for (let i = 0, n = parametro.length; i < sizeSenha; i++) {
+            Novasenha += parametro.charAt(Math.floor(Math.random() * n));
+        }
     }
+    function gerarSenhaCompleta(parametro) {
+        for (let i = 0, n = parametro.length; i < (senha.length - 2); i++) {
+            Novasenha += parametro.charAt(Math.floor(Math.random() * n));
+        }
+    }
+}
 
-    containerPassword.classList.remove("hide");
-    password.innerHTML = pass;
-    novaSenha = pass;
+function senhaNum(parametro) {
+    for (let i = 0, n = parametro.length; i < sliderElement.value; i++) {
+        Novasenha += parametro.charAt(Math.floor(Math.random() * n));
+    }
+    password.innerHTML = Novasenha;
+   Novasenha = "";
+}
+
+function senhaCompleta() {
+    console.log(sliderElement.value)
+    if (checkbox.checked == true) {
+        console.log("entrou")
+        gerarSenha(letrasMaisculas)
+        gerarSenha(letrasMinusculas)
+        gerarSenhaCompleta(charsetCompleto);
+        gerarSenha(numeros)
+        gerarSenha(caracterEspecial)
+
+        function gerarSenha(parametro) {
+            for (let i = 0, n = parametro.length; i < (sliderElement.value - sliderElement.value + 1); i++) {
+                Novasenha += parametro.charAt(Math.floor(Math.random() * n));
+            }
+        }
+
+        function gerarSenhaCompleta(parametro) {
+            for (let i = 0, n = parametro.length; i < (sliderElement.value - 4); i++) {
+                Novasenha += parametro.charAt(Math.floor(Math.random() * n));
+            }
+        }
+        password.innerHTML = Novasenha;
+        Novasenha="";
+    }
 }
 
 function copyPassword() {
 
-    navigator.clipboard.writeText(novaSenha);
+    navigator.clipboard.writeText(Novasenha);
 
-    alert(novaSenha + "\n\nSenha copiada!");
+    alert(Novasenha + "\n\nSenha copiada!");
 }
+
+
+
